f5a8136e4863fe63b7069d664d90ba430ad79380
getdn <getdna99@gmail.com>
2023-03-23 23:08:05 -0700

    OOBE
    Archive LeftFlyout

---
+++ UnitedSets/Windows/OOBE.xaml.cs
@@ -0,0 +1,15 @@
+﻿using UnitedSets.Windows.Pages;
+
+namespace UnitedSets.Windows;
+
+
+public sealed partial class OOBEWindow : MicaWindow
+{
+    public OOBEWindow()
+    {
+        this.InitializeComponent();
+        ExtendsContentIntoTitleBar = true;
+        SetTitleBar(AppTitleBar);
+        OOBE.Navigate(typeof(OOBEPage));
+    }
+}

fb0cebddbd85794f224f93536281b104d3f3e13f
Mitch Capper <mitch.capper@gmail.com>
2023-04-05 01:37:39 -0700

    refactored out several UWP related code blocks for use in the ability to save and restore UWP processes.

--- UnitedSets/Classes/OurHwndHost.cs
+++ UnitedSets/Classes/OurHwndHost.cs
@@ -10,6 +10,8 @@ using WindowEx = WinWrapper.Window;
 
 using EasyCSharp;
 using UnitedSets.Helpers;
+using System.IO;
+using Microsoft.Win32;
 
 namespace UnitedSets.Classes {
 	public interface IHwndHostParent {
@@ -31,18 +33,57 @@ namespace UnitedSets.Classes {
 		}
 
 		public string GetTitle() => host.HostedWindow.TitleText;
-		public string? GetOwnerProcessModuleFilename() {
-			var FileName = host.HostedWindow.OwnerProcess.GetDotNetProcess.MainModule?.FileName;
-			if (FileName == @"C:\WINDOWS\system32\ApplicationFrameHost.exe") {
-				var child = host.HostedWindow.Children.FirstOrDefault(x =>
-					x.ClassName is "Windows.UI.Core.CoreWindow", host.HostedWindow);
-				FileName = child.OwnerProcess.GetDotNetProcess.MainModule?.FileName;
+		public string? GetOwnerProcessModuleFilename() => GetOwnerWindow().OwnerProcess.GetDotNetProcess.MainModule?.FileName;
+		/// <summary>
+		/// Work around WinUI/UWP as AppFrameHost is normally the owner but we want the actual app
+		/// </summary>
+		/// <returns></returns>
+		protected WindowEx GetOwnerWindow(out bool wasUwp) {
+			var owner = host.HostedWindow;
+			wasUwp = false;
+			var mainModulePath = owner.OwnerProcess.GetDotNetProcess.MainModule?.FileName ?? "";
+			if (mainModulePath?.Equals(System.IO.Path.Combine(Environment.SystemDirectory, "ApplicationFrameHost.exe"), StringComparison.CurrentCultureIgnoreCase) != true) {
+				wasUwp = mainModulePath!.Contains(WindowsAppFolder ?? LoadWindowsAppFolder(), StringComparison.CurrentCultureIgnoreCase);//some windows apps dont use appframehost, IE windows terminal
+					
+				return owner;
 			}
-			return FileName;
+			wasUwp = true;
+			var child = GetCoreWindowFromAppHostWindow(host.HostedWindow);
+			return child;
 		}
+		public static WindowEx GetCoreWindowFromAppHostWindow(WindowEx appFrameHostMainWindow) => appFrameHostMainWindow.Children.FirstOrDefault(x => x.ClassName is "Windows.UI.Core.CoreWindow", appFrameHostMainWindow);
+		private static string? WindowsAppFolder = null;
+		private static string LoadWindowsAppFolder() {
+			if (WindowsAppFolder == null) {
+				using var appx = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Appx");
+				WindowsAppFolder = appx?.GetValue("PackageRoot")?.ToString() ?? "invalid";
+			}
+			return WindowsAppFolder;
+		}
+		public const string OUR_WINDOWS_STORE_APP_EXEC_PREFIX = "#WindowsApp!";
+		protected WindowEx GetOwnerWindow() => GetOwnerWindow(out _);
 		public (string cmd, string args) GetOwnerProcessInfo() {
-			var parse = ExternalProcessHelper.GetProcessCommandLineByPID(host.HostedWindow.OwnerProcess.Id.ToString());
-			return ExternalProcessHelper.ParseCmdLine(parse!);
+			var owner = GetOwnerWindow(out var wasUWP);
+			var toParse = ExternalProcessHelper.GetProcessCommandLineByPID(owner.OwnerProcess.Id.ToString());
+			var parsed = ExternalProcessHelper.ParseCmdLine(toParse!);
+			// So to get theofficial UWP executable we should use mainModulePath, as the command line can be different for example "wt" will launch windows terminal and will show that as its process. Now we could always use mainModulePath but as we can relaunch terminal with the same command we simply can honor what it says it is.  We must disable UWP mode though in this case so if we are not under the App Folder we can assume not uwp.
+			if (wasUWP && parsed.filename.Replace("\\", "/").Contains((WindowsAppFolder ?? LoadWindowsAppFolder()).Replace("\\", "/")) == false)
+				wasUWP = false;
+			if (wasUWP) {
+				var fileInfo = new FileInfo(parsed.filename);
+				var dir = fileInfo?.Directory?.Name ?? "";
+				parsed.filename = $"{OUR_WINDOWS_STORE_APP_EXEC_PREFIX}{dir}";
+				var serverNamePos = parsed.args.IndexOf("-ServerName:", StringComparison.CurrentCultureIgnoreCase);
+				if (serverNamePos != -1) {
+					var spaceAfter = parsed.args.IndexOf(" ", serverNamePos + 1);
+					var part1 = parsed.args.Substring(0, serverNamePos);
+					var part2 = "";
+					if (spaceAfter != -1)
+						part2 = parsed.args.Substring(spaceAfter + 1);//should have a space before servername anyway
+					parsed.args = part1 + part2;
+				}
+			}
+			return parsed;
 		}
 
 		[AutoNotifyProperty(OnChanged = nameof(CropTopChanged))]
@@ -58,11 +99,11 @@ namespace UnitedSets.Classes {
 		private int _CropBottom;
 		private void CropBottomChanged() => host.CropBottom = _CropBottom;
 
-		[AutoNotifyProperty(OnChanged=nameof(ActivateCropChanged))]
+		[AutoNotifyProperty(OnChanged = nameof(ActivateCropChanged))]
 		private bool _ActivateCrop;
 		private void ActivateCropChanged() => host.ActivateCrop = _ActivateCrop;
 
-		[AutoNotifyProperty(OnChanged =nameof(BorderlessWindowChanged))]
+		[AutoNotifyProperty(OnChanged = nameof(BorderlessWindowChanged))]
 		private bool _BorderlessWindow;
 		private void BorderlessWindowChanged() => host.BorderlessWindow = _BorderlessWindow;
 
@@ -72,7 +113,7 @@ namespace UnitedSets.Classes {
 		}
 		public void ClearCrop() {
 			_CropTop = _CropBottom = _CropLeft = _CropRight = 0;
-			RaisePropertyChanged(nameof(_CropTop),nameof(_CropBottom),nameof(_CropLeft),nameof(_CropRight));
+			RaisePropertyChanged(nameof(_CropTop), nameof(_CropBottom), nameof(_CropLeft), nameof(_CropRight));
 			host.ClearCrop();
 		}
 
@@ -97,13 +138,13 @@ namespace UnitedSets.Classes {
 		}
 		private void Host_Closed() {//this should only get called if the window handle is no longer valid or we called Close
 			if (_closed)
-                System.Diagnostics.Debug.WriteLine("OurHwndHost::Host_Closed: Already closed??");
-			if (! _clean_close)
+				System.Diagnostics.Debug.WriteLine("OurHwndHost::Host_Closed: Already closed??");
+			if (!_clean_close)
 				Closed?.Invoke(this, EventArgs.Empty);
 			_closed = true;
 		}
-		private bool _closed=false;
-		private bool _clean_close=false;
+		private bool _closed = false;
+		private bool _clean_close = false;
 		public event EventHandler? Closed;
 		public event EventHandler? Detached;
 		public void SetBorderless(bool borderless) => BorderlessWindow = borderless;

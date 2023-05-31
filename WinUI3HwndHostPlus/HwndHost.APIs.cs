be7d1a3be9e2094d4090bc68a7e8dd8a608a6f4c
getdn <getdna99@gmail.com>
2023-05-31 17:22:52 +0700

    Fix bug and add one more file

---
+++ WinUI3HwndHostPlus/HwndHost.APIs.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.UI.Dispatching;
+using Microsoft.Toolkit.Uwp;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace WinUI3HwndHostPlus;
+
+partial class HwndHost
+{
+    public partial Task DetachAndDispose(bool Focus = true);
+    public partial void FocusWindow();
+    public partial void Dispose();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public partial void ForceUpdateWindow();
+
+    public event Action? Closed;
+    public event Action? Updating;
+
+    public void FixSizeBug() => _HostedWindow.Restore();
+    public bool MayBeSizeBug =>
+        _CacheWidth == 0 &&
+        _CacheHeight == 0 &&
+        !IsDisposed &&
+        _HostedWindow.IsValid &&
+        _HostedWindow.IsNormalSize &&
+        Visibility == Microsoft.UI.Xaml.Visibility.Visible;
+}
